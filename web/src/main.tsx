bd5e04d3a4d9dba7befa2a398df0f056b041235f
Mayderson Mello <maydersonmello@gmail.com>
2026-04-02 00:29:30 -0300

    fix(web): rename file

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/react-query/query-client';
-import { App } from './app';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
